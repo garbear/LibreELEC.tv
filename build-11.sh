443d12a4c3bd74a7776b803cd406e85266eb261c
Garrett Brown <themagnificentmrb@gmail.com>
2023-07-12 18:26:00 -0700

    Add build-11.sh and build-12.sh

---
+++ build-11.sh
@@ -0,0 +1,4 @@
+PROJECT=Generic DEVICE=Generic ARCH=x86_64 CUSTOM_VERSION=retroplayer-20230709-c8bb2cfa make image
+PROJECT=RPi ARCH=arm DEVICE=RPi2 CUSTOM_VERSION=retroplayer-20230709-c8bb2cfa make image
+PROJECT=RPi ARCH=arm DEVICE=RPi4 CUSTOM_VERSION=retroplayer-20230709-c8bb2cfa make image
+PROJECT=Rockchip ARCH=arm DEVICE=RK3399 CUSTOM_VERSION=retroplayer-20230709-c8bb2cfa make image
