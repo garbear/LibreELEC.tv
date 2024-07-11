df1e5161cb4e1811dac3db8849434fc8d01f04cb
Garrett Brown <themagnificentmrb@gmail.com>
2024-07-11 02:17:05 -1000

    game.libretro.pcsx-rearmed: Update packages

--- packages/mediacenter/kodi-binary-addons/game.libretro.pcsx-rearmed/package.mk
+++ packages/mediacenter/kodi-binary-addons/game.libretro.pcsx-rearmed/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="game.libretro.pcsx-rearmed"
-PKG_VERSION="24.0.0.56-Nexus"
-PKG_SHA256="edd571b9c5f63fe666f9e83b606a6bc3b563d69a4919c1f77097ca26aa0e34a0"
-PKG_REV="2"
+PKG_VERSION="24.0.0.57-Nexus"
+PKG_SHA256="3ca5bad5d2e95049adb6fd5f92005a3f38f84c11ae2327de9502988ea1fc0a72"
+PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/kodi-game/game.libretro.pcsx-rearmed"
